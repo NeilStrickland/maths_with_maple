dd4c6ce356634cd457d1db191e33915835242387
Neil Strickland <N.P.Strickland@sheffield.ac.uk>
2020-05-05 12:25:15 +0100

    Fix references to defineq and ratint

---
+++ scripts/make_docs.sh
@@ -0,0 +1,12 @@
+cp ../all_exams/all_exams.pdf ../docs/all_exams
+cp ../labs/primer.pdf ../docs/labs
+cp ../labs/all_labs.pdf ../docs/labs
+cp ../lab_sols/lab_sols*.mw ../docs/lab_sols
+cp ../lab_sols/pdf/lab_sols*.pdf ../docs/lab_sols/pdf
+cp ../lab_setup/defineq.mw ../docs/lab_setup
+cp ../lab_setup/ratint.mw ../docs/lab_setup
+cp ../lectures/all_handouts.pdf ../docs/lectures
+cp ../lectures/all_lectures.pdf ../docs/lectures
+cp ../presentations/presentation*.mw ../docs/presentations
+cp ../presentations/presentation*.pdf ../docs/presentations
+cp ../tutorials/all_probs.pdf ../docs/tutorials
